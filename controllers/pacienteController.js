4e068f1eafa22d55783b6b7002df9b2a4662ae84
jromo1506 <jromo1506mx@gmail.com>
2025-01-03 10:15:05 -0600

    Commit

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -1,16 +1,27 @@
 const Paciente = require('../models/Paciente.js');
 
 
-exports.addPaciente = async(req,res)=>{
-
+exports.addPaciente = async(req,res)=>{ 
+    try{
+        const nuevoPac = new Paciente(req.body);
+        res.status(201).json(await nuevoPac.save());
+    }
+    catch(error){
+        console.log("Error al crear un paciente");
+    }
 }
 
 
 exports.getPacientes = async(req,res)=>{
-
+  
 }
 
 
 exports.getPaciente = async(req,res) => {
-    
+
+}
+
+
+exports.putPaciente = async(req,res) =>{
+
 }
