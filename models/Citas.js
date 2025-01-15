5030e72de7db0f64bcd15163a7d0f509c04cc5da
Fer3Orenday <fer.orenday3@gmail.com>
2025-01-15 13:30:53 -0600

    Cita

--- models/Citas.js
+++ models/Citas.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const CitasSchema = new Schema({
-    
-
+    id:{
+        type: String,
+        required: false,
+    },
     tratamiento: {
         type: String,
         required: true,
@@ -16,9 +18,22 @@ const CitasSchema = new Schema({
         type:Number,
         required:false,
     },
+    horaInicio:{
+        type:String,
+        required:false,
+    },
+    horaFin:{
+        type:String,
+        required:false,
+    },
     realizo:{
         type:String,
         required:false
+    },
+    pacienteId: { // Este es el campo que se referirá al paciente
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Paciente', // Esto indica que el campo pacienteId hace referencia al modelo 'Paciente'
+        required: true // Este campo es obligatorio
     }
 });
 
