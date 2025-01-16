f6c6bb324b6f3f1d03a2a0e8c401d269caf9f878
WallaceGaster <saitama.kurosakyy@gmail.com>
2025-01-16 14:08:00 -0600

    actualizando

--- models/Citas.js
+++ models/Citas.js
@@ -2,20 +2,11 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const CitasSchema = new Schema({
-<<<<<<< HEAD
     idPaciente:{
         type:String,
         required:true
     },
 
-=======
-    
-    pacienteId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Paciente',
-        required: true
-    },
->>>>>>> d205aa2c62ea6996936118f394a6b6c3aef20ed2
     tratamiento: {
         type: String,
         required: true,
