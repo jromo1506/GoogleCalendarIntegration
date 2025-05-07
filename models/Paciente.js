3fd8f5d88dbe2c9e79800df9a801c784dd7f632c
WallaceGaster <saitama.kurosakyy@gmail.com>
2025-05-07 15:42:56 -0600

    actualizando modelo de pacientes

--- models/Paciente.js
+++ models/Paciente.js
@@ -45,10 +45,6 @@ const PacienteSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    nombreTutor: {
-        type: String,
-        required: false,
-    },
 
     altura: {
         type: Number,
